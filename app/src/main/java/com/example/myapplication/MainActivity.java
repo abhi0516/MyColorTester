c5e860e59f78d17986a8c4224f31b7b88846132c
abhi0516 <90794084+abhi0516@users.noreply.github.com>
2022-09-15 20:55:03 -0500

    switch screen commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -15,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void switchScreens(View v) {
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(intent);
+    }
+
 //    EditText userAddress = findViewById(R.id.userAddress);
 //    EditText userFirstName = findViewById(R.id.userFirstName);
 //    EditText userLastName = findViewById(R.id.userLastName);
@@ -23,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
 //    EditText userZip = findViewById(R.id.userZIP);
 //
 //    public void displayInfo(View view) {
-
         // I want to display all of user's personal info and billing info on second screen(billing screen)
         // Program should switch screens once CONTINUE button is pressed
 
@@ -32,13 +37,4 @@ public class MainActivity extends AppCompatActivity {
         //add info to string
         //set texview = to that string
 
-
-
-//        double num1 = Double.parseDouble(number1ET.getText().toString());
-//        double num2 = Double.parseDouble(number2ET.getText().toString());
-//        double sum = num1 + num2;
-//        double roundedSumValue = (double) Math.round(sum * 100) / 100;
-//        numberSumTV.setText("   " + roundedSumValue);
-//    }
-//
 }
