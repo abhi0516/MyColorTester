94b2d9e4974028eb0109521d4b65608d2885c101
abhi0516 <90794084+abhi0516@users.noreply.github.com>
2022-09-16 12:54:59 -0500

    review screen activity commit

--- app/src/main/java/com/example/myapplication/ReviewInfoScreen.java
+++ app/src/main/java/com/example/myapplication/ReviewInfoScreen.java
@@ -6,16 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity2 extends AppCompatActivity {
+public class ReviewInfoScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_review_info_screen);
     }
 
-    public void switchScreens(View v) {
-        Intent intent = new Intent(MainActivity2.this, ReviewInfoScreen.class);
-        startActivity(intent);
-    }
+
 }
